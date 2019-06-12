1b3223c6ce907d74b6d884312e646609bef122a5
Sahaana Thangadurai <sahaana.thangadurai04@gmail.com>
2019-06-12 11:47:21 -0500

    Added function exercises

---
+++ functions_lab.py
@@ -0,0 +1,18 @@
+"""
+title: functions_lab
+author: Sahaana
+date: 2019-06-12 11:22
+"""
+
+def age_calculator(year1, year2):
+    return year1 - year2
+
+
+print(age_calculator(2019, 2003))
+
+
+def avg_numbers(x1, x2, x3):
+    return (x1+x2+x3)/3
+
+
+print(avg_numbers(3, 4, 5)) 
